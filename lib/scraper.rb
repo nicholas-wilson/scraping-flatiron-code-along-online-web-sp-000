6c86c1293f38cfec7becdc190e528f6d184ba39c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-01 21:37:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,12 +18,14 @@ class Scraper
   end
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-
+    scraping/courses"))
 
     binding.pry
   end
 
   def get_courses
+    courses = doc.css(".post")
   end
 
   def make_courses
